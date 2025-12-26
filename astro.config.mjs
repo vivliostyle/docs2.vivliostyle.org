e53019d5220d0b6646a4ecc373c0b7a480951cc8
小形克宏 <ogwata@vivliostyle.org>
2025-12-26 20:32:16 +0900

    fix: 本番公開URLに合わせてastro.config.mjsのsite/baseを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vivliostyle.github.io',
-  base: '/docs2.vivliostyle.org',
+  site: 'https://docs2.vivliostyle.org',
   output: 'static',
   build: {
     format: 'directory',
